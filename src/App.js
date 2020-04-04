bb8ff5c231557dbc6415d534a5905f12986643dc
Jim Haff <Jimmyhaff3@gmail.com>
2020-04-04 17:25:51 -0500

    grid added

--- src/App.js
+++ src/App.js
@@ -12,6 +12,19 @@ class App extends Component {
 
 
   render(){
+    // setup the router from react router
+    // this is where you define the routes
+    // Make a header component, that has links to the maincontainer
+    // and myabe an about page, that will be rendered on every page
+    // a route for MainContainer
+    // a route for an About Page
+
+    // Landing Page, localhost:3000 this page accepts an input
+    // with the users name, Which will stored in app.js state
+    // then you'll direct the user to the mainContainer which is different
+    // route
+    // and in the maincontainer pass it the prop of the persons
+    // username and leave a message
     return (
       <React.Fragment>
         <MainContainer />
