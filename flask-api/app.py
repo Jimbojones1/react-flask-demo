b827f47d4806569df40fac357d3f03667f091831
Jim Haff <Jimmyhaff3@gmail.com>
2020-04-04 12:47:31 -0500

    youtube api working

---
+++ flask-api/app.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask import jsonify
+from googleapiclient.discovery import build
+import os
+
+app = Flask(__name__)
+
+
+API_KEY = os.environ.get("YOUTUBE_API_KEY")
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+# def search_by_keyword():
+youtube = build(
+  YOUTUBE_API_SERVICE_NAME,
+  YOUTUBE_API_VERSION,
+  developerKey=API_KEY
+)
+
+@app.route('/search/<term>')
+def get_youtube_videos(term):
+
+    search_response = youtube.search().list(
+      q=term,
+      part="snippet",
+      maxResults=10
+    ).execute()
+
+    print(search_response)
+
+    return jsonify(data=search_response)
+
+
+
+
